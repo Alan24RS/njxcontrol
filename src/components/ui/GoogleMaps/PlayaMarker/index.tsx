b785d3b4e776b7756724111419bd5f053e914944
OjedaJuanAntonio <juancho.ojeda11@gmail.com>
2025-11-30 20:17:03 -0300

    feat: mejorar diseño del marcador de playa con forma de gota y badge de disponibilidad

--- src/components/ui/GoogleMaps/PlayaMarker/index.tsx
+++ src/components/ui/GoogleMaps/PlayaMarker/index.tsx
@@ -1,6 +1,6 @@
-import { AdvancedMarker, AdvancedMarkerProps } from '@vis.gl/react-google-maps'
+import Image from 'next/image'
 
-import LogoMarker from '@/assets/institutional/LogoMarker'
+import { AdvancedMarker, AdvancedMarkerProps } from '@vis.gl/react-google-maps'
 
 interface PlayaMarkerProps extends AdvancedMarkerProps {
   children?: React.ReactNode
@@ -9,9 +9,40 @@ interface PlayaMarkerProps extends AdvancedMarkerProps {
 export default function PlayaMarker({ children, ...props }: PlayaMarkerProps) {
   return (
     <AdvancedMarker {...props}>
-      <div className="relative">
-        <LogoMarker className="size-12" />
-        {children}
+      <div className="relative inline-block">
+        {/* Badge de disponibilidad - centrado arriba de la gota */}
+        {children && (
+          <div className="absolute -top-6 left-1/2 z-20 -translate-x-1/2">
+            {children}
+          </div>
+        )}
+        {/* Contenedor con forma de gota */}
+        <div className="relative flex items-center justify-center">
+          {/* SVG de fondo con forma de gota */}
+          <svg
+            width="48"
+            height="64"
+            viewBox="0 0 48 64"
+            className="block"
+            style={{ filter: 'drop-shadow(0 4px 6px rgba(0, 0, 0, 0.3))' }}
+          >
+            <path
+              d="M24 0C10.745 0 0 10.745 0 24c0 13.255 24 40 24 40s24-26.745 24-40C48 10.745 37.255 0 24 0z"
+              fill="currentColor"
+              className="text-primary"
+            />
+          </svg>
+          {/* Ícono dentro de la gota */}
+          <div className="absolute top-3 left-1/2 -translate-x-1/2">
+            <Image
+              src="/favicon.ico"
+              alt="Playa"
+              width={28}
+              height={28}
+              className="rounded-sm"
+            />
+          </div>
+        </div>
       </div>
     </AdvancedMarker>
   )
